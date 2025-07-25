43be32a8682a758bd11e4e5f1a58e5a31482e82c
andrea-c2 <a.caronni2@campus.unimib.it>
2025-07-25 02:10:45 +0200

    Add files via upload
    aggiunta normalize (sarebbe da rinominare) e diverse funzioni ausiliari.
    dovrebbero esserci tutte le funzioni ma vanno testate soprattutto le qadd qsub ecc.

--- siunits.lisp
+++ siunits.lisp
@@ -66,7 +66,7 @@
     (|lm| . lumen)
     (|lx|. lux)
     (|N| . Newton)
-    (|omega| .Ohm)
+    (|omega| . Ohm)
     (|Pa| . Pascal)
     (|rad| . radian)
     (|S| . Siemens)
@@ -123,8 +123,16 @@
   (mapcar 'make-symbol
 	  (mapcar
 	   (lambda (x)(concatenate 'string x
-				   (string-downcase
-				    (string (si-unit-symbol unit)))))
+				    (string (si-unit-symbol unit))))
+	   (mapcar 'string (mapcar 'car si-prefixes)))))
+
+;dato un simbolo, restituisce la lista con tutti i prefissi applicati al
+;simbolo. Da usare con g (grammo).
+(defun make-symbol-prefixes (symb)
+    (mapcar 'make-symbol
+	  (mapcar
+	   (lambda (x)(concatenate 'string x
+				    (string symb)))
 	   (mapcar 'string (mapcar 'car si-prefixes)))))
 
 ;ritorna T se il suo argomento e' un simbolo (la sigla) delle unita' di base
@@ -223,35 +231,91 @@
         ((< idx1 idx2) '<)
         (t '>)))))
 
+;ritorna T se qt e' una quantita' (ovvero un numero)
+
+(defun is-quantity (q)
+  (and (listp q)
+       (eq 'q (first q))
+       (let ((n (second q))
+	     (d (third q))
+	     )
+	 (and (numberp n)
+	      (is-dimension d)))))
+
 ;ritorna T se dim e' una dimensione, ovvero un simbolo di unita' base o derivat
 ;a oppure una lista con operatore * e operandi unita' o espressioni
 ;(expt u e)
+;NB (expt m 2) non e' valido (da solo), (* (expt m 2 )) si.
 
-;(defun is-dimension (dim)
-;  (cond ((is-si-unit dim) t)
-;	(and (listp dim)
-;	     (equal * (first dim))
-;	     ())))
+(defun is-dimension (dim)
+  (cond ((and (not (listp dim))(not (null (decompose-si-unit dim))) t))
+	((and (listp dim)) (equal (first dim) '*)
+	 (and-list (mapcar 'is-unit-operand
+			      (rest dim))))))
+
+;restituisce t se tutti gli elementi di una lista sono T,altrimenti NIL.
+
+(defun and-list (boolist)
+  (cond ((null boolist) T)
+	(t (and (car boolist) (and-list (rest boolist))))))
 
 ;ritorna T se dim e' un unita' SI (multiplo o non) o se e' una espressione
-;del tipo (expt u e) con u unita' si (multiplo o non) ed e numero
+;del tipo (expt u e) con u unita' si (multiplo o non) ed e
 
-;(defun is-unit-operand (operand)
-;  ())
+(defun is-unit-operand (operand)
+  (cond ((listp operand) (and (equal (first operand) 'expt)
+			     (not (null (decompose-si-unit (second operand))))
+			     (numberp (third operand))))
+	((not (null (decompose-si-unit operand))) t)))
 
-;ritorna l'unita con prefisso se l'argomento e' un unita' con prefisso
+;restituisce l'unita' se l argomento e' un unita' base e derivata senza prefisso
+;se ha il prefisso restituisce ('unitabase expt 10 val) altrimenti rest NIL.
+
+(defun decompose-si-unit (dim)
+  (cond ((equal dim '|kg|) dim)
+	((is-si-unit dim) dim)
+	((not (null (check-prefixed-si-unit dim)))
+	(cons (make-symbol (check-prefixed-si-unit dim))
+	      (rest (car
+		     (decompose-prefixed-si-unit dim
+					    (check-prefixed-si-unit dim))))))))
+
+;restituisce simbolo pref e potenza di dieci dato argomento unita' con prefisso
+;e unita' corrispondente senza prefisso es. cm m, microomega omega
+;NB decompone kg in g * 10 exp 3
+
+(defun decompose-prefixed-si-unit (prefixed base)
+  (remove-if-not (lambda (x)
+	       ;cerca il prefisso
+		   (and (not (null x))
+			(equal (string (car x))
+		      (subseq (string prefixed) 0
+			      (min (length (string prefixed))
+				   (length (string (car x))))))
+	       ;assicurati che dopo il prefisso ci sia l'unita'
+		    (equal (string base)
+			   (subseq
+			    (string prefixed)
+			    (min (length (string prefixed))
+				  (length (string (car x))))))))
+	     si-prefixes))
+
+;ritorna l'unita senza prefisso se l'argomento e' un unita' con prefisso
 ;altrimenti NIL.
 ;prima e' saggio verificare che l'argomento sia un simbolo si senza prefisso.
+;NB se l'argomento e' kg restituisce g.
 
 (defun check-prefixed-si-unit (dim)
-	;ottieni la lista di tutti i simboli di unita SI che compaiono in dim
-	;per ciascuno di questi simboli, vedi se riesci a trovare dim uguale a
-	;uno dei suoi prefissi, se lo trovi, allora dim e' un unita con prefix
-  (first (remove-if 'null (mapcar (lambda (x)
-				   (find-if (lambda (y) (equal y (string dim)))
-					    (mapcar 'string (make-unit-prefixes
-					     (si-unit-name (make-symbol x))))))
-		 (find-units-list (string dim))))))
+  (first (remove-if
+	  (lambda (x)
+	    (null (find-if
+		   (lambda (y) (equal y (string dim)))
+		   (mapcar 'string (if (equal x "g")
+				      (make-symbol-prefixes (make-symbol x))
+				      (make-unit-prefixes
+				       (si-unit-name
+					(make-symbol x))))))))
+	   (find-units-list (string dim)))))
 
 ;ritorna una lista contenente tutti i simboli unita si senza prefissi
 ;che compaiono nella stringa argomento
@@ -260,27 +324,124 @@
   (remove-if (lambda (x) (null(search x dim)))
 	     ;sostituisci kg con g
 	     (substitute-if "g" (lambda (x) (equal x "kg"))
-			     (mapcar 'string-downcase
-				     (mapcar 'string
-					 (mapcar 'car units-symbol-name))))))
+			    (mapcar 'string
+				    (mapcar 'car units-symbol-name)))))
 
 ; Normalizzazione delle dimensioni
 
-(defun normalize (dim)
+;(defun normalize (dim)
 ;Normalizza una lista di unit√†: somma esponenti, ordina, rimuove esponenti
 ;nulli.
-  (let ((table (make-hash-table :test #'equal)))
-    (dolist (pair dim)
-      (let* ((u (car pair))
-             (e (cdr pair))
-             (old (gethash u table 0)))
-        (setf (gethash u table) (+ old e))))
-    (let (result)
-      (maphash (lambda (u e)
-                 (unless (= e 0)
-                   (push (cons u e) result)))
-               table)
-      (sort result #'string< :key (lambda (x) (symbol-name (car x)))))))
+;  (let ((table (make-hash-table :test #'equal)))
+;    (dolist (pair dim)
+;      (let* ((u (car pair))
+;             (e (cdr pair))
+;             (old (gethash u table 0)))
+;        (setf (gethash u table) (+ old e))))
+;    (let (result)
+;      (maphash (lambda (u e)
+;                 (unless (= e 0)
+;                   (push (cons u e) result)))
+;               table)
+;      (sort result #'string< :key (lambda (x) (symbol-name (car x)))))))
+
+
+;NB normalize tratta unita che differiscono solo per il prefisso come se
+;fossero uguali es. (* (expt |cm| 1) (expt |m| 1)) diventa (* (expt |m|
+
+(defun normalize (dim)
+  ;se dim e' un unita' con o senza prefisso restituisce (* dim)
+  (cond ((and
+	  (not (listp dim))
+	  (or (is-si-unit dim)
+	      (not (null (check-prefixed-si-unit dim)))))
+	 (list '* dim))
+	 ((and (listp dim) (equal '* (first dim))
+	       (cons '*
+		     (simplify-base-units-list (sort (mapcar 'first
+			     (sum-base-units
+			      (mapcar 'expt-base-unit
+				      (rest dim))))
+					(lambda (x y) (string<=
+					  (string-downcase (string (second x)))
+				       	    (string-downcase
+						  (string (second y))))))))))))
+
+;prende come argomento una lista di (expt u e) e restituisce una lista con
+;alcune modiche. per ogni elemento della lista, se e = 1, sostituisci con
+;solo l'unita', se e = 0, rimuovi.
+
+(defun simplify-base-units-list (ls)
+  (mapcar
+   (lambda (x)
+     (cond ((equal 1 (third x)) (second x)) (t x)))
+   (remove-if (lambda (x) (equal 0 (third x))) ls)))
+
+;prende come argomento una lista di ((expt u e) esp) (ottenuta da fz
+;expt-base-unit. Prende tutte gli elementi corrispondenti ad un unita si e
+;calcola il prodotto. es. ((expt |m| 2) 2) ((expt |m| 1) 2) diventa
+;((expt |m| 3) 4)
+
+(defun sum-base-units (ls)
+  (let ((base-units
+	 (remove-duplicates (mapcar (lambda (x) (second (first x))) ls)
+			    :test (lambda (x y)
+				    (equal (string x) (string y))))))
+  (mapcar 'sum-same-base-units
+	  (mapcar (lambda (x)
+		    (remove-if-not
+		     (lambda (y)
+		       (equal (string x) (string (second (first y)))))
+		     ls))
+	  base-units))))
+
+;prende come argomento una lista di ((expt u e) esp) e da per scontato che siano
+;tutte della stessa unita'.
+;calcola il prodotto. es. ((expt |m| 2) 2) ((expt |m| 1) 2) diventa
+;((expt |m| 3) 4)
+
+(defun sum-same-base-units (ls)
+  (if (not (null ls))
+      (list (list 'expt (second (first (first ls)))
+		  (reduce #'+ (mapcar (lambda (x) (third (first x))) ls)))
+			    (reduce #'* (mapcar (lambda (x) (second x)) ls)))))
+
+
+;se l'argomento È un unita' base restituisce (expt dim 1)
+;se l'argomento È un unita' con prefisso rest. ((expt dim 1) esp) dove esp
+;e' l'esponente corrispondente al prefisso.
+
+;se l'argomento È una lista (expt u e) restituisce
+;((expt u1 e) num) dove u1 e' l' unita base corrispondente a u
+;e num e' la potenza di dieci per fare la conversione tra unita' mantenendo la
+;quantita' equivalente es. (expt cm 1) diventa ((expt m 1) -2)
+;per calcolare num bisogna moltiplicare e per l'esponente corrispondente al
+;prefisso es (expt |cm| 3) diventa ((expt |m| 3) -6) ovvero -2 * 3.
+
+(defun expt-base-unit (dim)
+  (cond
+    ((listp dim) (let ((ex (first dim)) (u (second dim)) (e (third dim))
+		       (ubase
+			       ((lambda (x) (if (is-si-unit x) x
+						(check-prefixed-si-unit
+						  x)))
+				(second dim))))
+		   (if (and (equal ex 'expt) (numberp e))
+		       (cond ((is-si-unit u) (list dim 1))
+			     ((not (null ubase))
+			      (list (list 'expt ubase e)
+				    (* e (car (last
+					       (first
+						(decompose-prefixed-si-unit
+						 u ubase)))))))))))
+    ((is-si-unit dim) (list (list 'expt dim 1) 1))
+    (t (let ((ubase (make-symbol (check-prefixed-si-unit dim))))
+	 (if
+	  (not (null ubase))
+	  (list (list 'expt ubase 1)
+		(car
+		 (last
+		  (first (decompose-prefixed-si-unit dim ubase))))))))))
 
 ; Operazioni tra quantit√†
 
